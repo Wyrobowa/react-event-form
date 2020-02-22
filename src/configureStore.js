e13340ff2d24cd9be2e8a4b5999089c897ee5373
aniawyroba <aniawyroba@gmail.com>
2020-02-22 22:18:42 +0100

    removed dotenv and .env file, added condition for creating store depending on environment

--- src/configureStore.js
+++ src/configureStore.js
@@ -3,6 +3,16 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import reducer from './reducers/index';
 
-const configureStore = () => createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
+const configureStore = () => {
+  let store;
+
+  if (process.env.NODE_ENV === 'development') {
+    store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
+  } else {
+    store = createStore(reducer, applyMiddleware(thunk));
+  }
+
+  return store;
+};
 
 export default configureStore;
